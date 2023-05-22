1e9a2f0af736a4f8aa7a0336b6f5c11ae079729b
Mickael Breteau <mickael.breteau@hotmail.fr>
2023-05-22 17:00:28 +0200

    Formulaire en cours, FAQ quasi terminé

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-($=>{
+($ => {
     $(() => {
         const popin = $('.popin')
         const contactForm = $('#contact', popin)
         $('.accordion-title').on('click', (e) => {
-            $('.accordion-section').removeClass('open')
-            $(e.currentTarget).parent().addClass('open')
+            const accordionSection = $(e.currentTarget).parent()
+
+            if (accordionSection.hasClass('open')) {
+                accordionSection.removeClass('open')
+            } else {
+                $('.accordion-section').removeClass('open')
+                accordionSection.addClass('open')
+            }
         })
+
         $('.more-visibility').on('click', (e) => {
             const currentItem = $(e.currentTarget)
             const popinTitle = $('.popin-title')
             const theme = currentItem.data('theme')
             const popinTitleValue = currentItem.data('title')
-    
+
             popinTitle.html(popinTitleValue)
             popin.addClass(theme).addClass('open')
         })
@@ -21,9 +28,8 @@
         contactForm.on('submit', e => {
             const firstname = $('input[name=firstname]').val()
             const num = /[0-9]+/ig
-            firstname.match(num) ?  console.log('success') : console.log('fail')
+            firstname.match(num) ? console.log('success') : console.log('fail')
             return false
-          
         })
     })
 })(jQuery)
