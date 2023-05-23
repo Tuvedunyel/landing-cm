9f951cb906713d34688219a15b3e006b5eb41b63
Mickael Breteau <mickael.breteau@hotmail.fr>
2023-05-23 17:01:13 +0200

    Test sur formulaire, manque des éléments

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
     $(() => {
         const popin = $('.popin')
         const contactForm = $('#contact', popin)
+        const body = $('body')
+        const popinRemerciement = $('.popin-remerciement')
+        let theme
         $('.accordion-title').on('click', (e) => {
             const accordionSection = $(e.currentTarget).parent()
 
@@ -16,20 +19,57 @@
         $('.more-visibility').on('click', (e) => {
             const currentItem = $(e.currentTarget)
             const popinTitle = $('.popin-title')
-            const theme = currentItem.data('theme')
+            theme = currentItem.data('theme');
             const popinTitleValue = currentItem.data('title')
+            const popinLogo = $('.popin-logo')
+            const popinLogoSrc = currentItem.data('image')
 
             popinTitle.html(popinTitleValue)
+            popinLogo.attr('src', popinLogoSrc);
             popin.addClass(theme).addClass('open')
+            popinRemerciement.addClass(theme)
+            body.addClass('popin-open')
+            return theme
         })
-        $('.popin-close').on('click', () => {
-            popin.removeClass('open')
-        })
-        contactForm.on('submit', e => {
-            const firstname = $('input[name=firstname]').val()
+
+        $('.popin, .popin-close, .popin-remerciement').on('click', (e) => {
+            if ($(e.target).hasClass('popin') || $(e.target).hasClass('popin-close') || $(e.target).hasClass('popin-remerciement')) {
+                popin.removeClass('open').removeClass(theme)
+                popinRemerciement.removeClass('open').removeClass(theme)
+                body.removeClass('popin-open')
+            }
+        });
+
+        $('#phone').on('input', function () {
+            let phoneNumber = $(this).val().replace(/\s/g, ''); // Supprimer les espaces existants
+            let formattedPhoneNumber = '';
+
+            for (let i = 0; i < phoneNumber.length; i++) {
+                formattedPhoneNumber += phoneNumber.charAt(i);
+
+                if ((i + 1) % 2 === 0 && i !== phoneNumber.length - 1) {
+                    formattedPhoneNumber += ' ';
+                }
+            }
+
+            $(this).val(formattedPhoneNumber);
+        });
+        $('input[name=phone]').on('input', function() {
+            const phone = $(this).val();
+            console.log(phone);
+          });
+
+        contactForm.on('submit', (e) => {
+            e.preventDefault()
+            const phone = $('input[name=phone]').val()
             const num = /[0-9]+/ig
-            firstname.match(num) ? console.log('success') : console.log('fail')
-            return false
+            console.log(phone)
+            if (!phone.match(num)) {
+                $('input[name=phone]').get(0).setCustomValidity('Le numéro de téléphone est invalide.')
+            } else {
+                popinRemerciement.addClass('open')
+                popin.removeClass('open')
+            }
         })
     })
 })(jQuery)
